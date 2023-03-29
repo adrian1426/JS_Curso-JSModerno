e780f339d06c7d7a2290948e40eef5d17e711fac
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-03-29 13:31:15 -0600

    creando barajas de cartas

--- blackjack/assets/js/juego.js
+++ blackjack/assets/js/juego.js
@@ -1,1 +1,23 @@
-console.log('juego')
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'K', 'Q'];
+
+const crearDeck = () => {
+  for (let i = 2; i < 10; i++) {
+    for (const tipo of tipos) {
+      deck.push(`${i}${tipo}`);
+    }
+  }
+
+  for (const tipo of tipos) {
+    for (const especial of especiales) {
+      deck.push(`${especial}${tipo}`);
+    }
+  }
+
+  deck = _.shuffle(deck);
+
+  return deck;
+};
+
+crearDeck();
