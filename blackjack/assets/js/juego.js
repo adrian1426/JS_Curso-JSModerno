3c686704600dc05a7eb2c05130cb5cf2980da042
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-04-11 10:56:40 -0600

    extrayendo valor número de las cartas

--- blackjack/assets/js/juego.js
+++ blackjack/assets/js/juego.js
@@ -25,5 +25,20 @@ const pedirCarta = () => {
   return carta;
 };
 
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  let puntos = 0;
+
+  if (isNaN(valor)) {
+    puntos = valor === 'A' ? 11 : 10;
+  } else {
+    puntos = Number(valor);
+  }
+
+  return puntos;
+};
+
+
 crearDeck();
-console.log("carta: ", pedirCarta());
+const carta = pedirCarta();
+const valor = valorCarta(carta);
