afa78e4ce87fa435a2abd4651014fd8fa5cb02c1
adrianhh1426 <adrianhh1426@gmail.com>
2023-07-02 22:01:58 -0600

    agregando singleton

--- clasesjs/Area.js
+++ clasesjs/Area.js
@@ -1,7 +1,15 @@
 class Rectangulo {
+
+  static instance;
   #area = 0;
 
   constructor(base = 0, altura = 0,) {
+    if (!!Rectangulo.instance) {
+      return Rectangulo.instance;
+    }
+
+    Rectangulo.instance = this;
+
     this.base = base;
     this.altura = altura;
     this.#area = base * altura;
@@ -9,4 +17,6 @@ class Rectangulo {
 }
 
 const rectangulo = new Rectangulo(10, 12);
+const rectangulo2 = new Rectangulo(12, 15);
 console.log(rectangulo);
+console.log(rectangulo2);
