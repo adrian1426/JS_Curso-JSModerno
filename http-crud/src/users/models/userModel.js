cc5cee877bec1ef1b283ad6f44f2ca20a8d675a2
adrianhh1426 <adrianhh1426@gmail.com>
2023-08-15 17:41:49 -0600

    creando models

---
+++ http-crud/src/users/models/userModel.js
@@ -0,0 +1,11 @@
+export class UserModel {
+  constructor(user) {
+    this.id = user.id;
+    this.isActive = user.isActive;
+    this.balance = user.balance;
+    this.avatar = user.avatar;
+    this.firstName = user.firstName;
+    this.lasName = user.lastName;
+    this.gender = user.gender;
+  }
+}
