d062408057298be58583634fa4c633debf4c2993
adrianhh1426 <adrianhh1426@gmail.com>
2023-08-25 13:51:24 -0600

    agregando babel

--- webpack-base/webpack.prod.js
+++ webpack-base/webpack.prod.js
@@ -35,6 +35,16 @@ module.exports = {
             loader: 'file-loader',
           },
         ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
